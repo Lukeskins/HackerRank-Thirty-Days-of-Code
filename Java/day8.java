66096942f9a08621b631403d10108305c9ea3aee
lukeskins <luke@lukeskinner.me>
2017-01-12 18:51:16 -0800

    Add Day 8 :tada:

---
+++ Java/day8.java
@@ -0,0 +1,31 @@
+// Day 8 - Dictionaries and Maps
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+public static void main(String [] argh){
+        Scanner in = new Scanner(System.in);
+        HashMap<String, Integer> phoneBook = new HashMap<>();
+        int n=in.nextInt();
+        in.nextLine();
+
+        for(int i=0; i<n; i++) {
+                String name=in.next();
+                int phone=in.nextInt();
+                phoneBook.put(name, phone);
+                in.nextLine();
+        }
+        while(in.hasNext()) {
+                String s = in.next();
+                // Write code here
+
+                if (phoneBook.containsKey(s)) {
+                        System.out.println(s+"="+phoneBook.get(s));
+                } else {
+                        System.out.println("Not found");
+                }
+        }
+        in.close();
+}
+}
