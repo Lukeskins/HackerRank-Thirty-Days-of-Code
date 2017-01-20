d558bcb039389f874d22524c8068f96a7895bdc1
lukeskins <luke@lukeskinner.me>
2017-01-19 17:24:10 -0800

    Minor formatting fix :art:

--- Java/day15.java
+++ Java/day15.java
@@ -1,20 +1,19 @@
 // Day 15 - Linked List
 
-class Solution {
-public static  Node insert(Node head, int data) {
-     Node n = new Node(data);
+public static Node insert(Node head, int data) {
+        Node n = new Node(data);
 
-    if (head == null) {
-        head = n;
+        if (head == null) {
+                head = n;
+                return head;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+                temp = temp.next;
+        }
+
+        temp.next = n;
+
         return head;
-    }
-
-    Node temp = head;
-    while (temp.next != null) {
-        temp = temp.next;
-    }
-
-    temp.next = n;
-
-    return head;
 }
