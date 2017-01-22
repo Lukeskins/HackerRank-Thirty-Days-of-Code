a7c364869321506f20f824b9bc78283eb00279ab
lukeskins <luke@lukeskinner.me>
2017-01-21 21:06:47 -0800

    Day 17 completed :tada:

---
+++ Java/day17.java
@@ -0,0 +1,16 @@
+// Day 17 - More Exceptions
+
+class Calculator {
+
+int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+                throw new Exception("n and p should be non-negative");
+        }
+
+        int total = 1;
+        for (int i = 0; i < p; i++) {
+                total *= n;
+        }
+        return total;
+}
+}
