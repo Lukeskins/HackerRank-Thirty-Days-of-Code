53ecb29c7c11e0d7cede44f6f5ddd7720bb0f7f3
lukeskins <luke@lukeskinner.me>
2017-01-31 18:02:09 -0800

    Day 27 complete :tada:

---
+++ Java/day27.java
@@ -0,0 +1,42 @@
+// Day 27 - Testing
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+
+        System.out.println(5);
+
+
+        System.out.println("300 400");
+
+        for (int i=-99; i<=100; i++) {
+                System.out.print(i + " ");
+
+        }
+        System.out.println();
+
+
+
+        System.out.println("3 2");
+        System.out.println("-1000 0 1000");
+
+
+        System.out.println("4 3");
+        System.out.println("-1 0 1 1000");
+
+
+        System.out.println("5 2");
+        System.out.println("-1000 0 -1 2 1000");
+
+
+        System.out.println("6 3");
+        System.out.println("1 -4 3 4 -1 0");
+
+}
+}
